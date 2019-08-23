b9321feb43e8db8e1b5954b9b03369a8f466b66c
Nikhil Singh <nikhilsingh498@gmail.com>
2019-08-23 21:16:28 +0530

    Toggle button update

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,12 +1,17 @@
 
 document.getElementById("navbar-btn").addEventListener("click", (e) => {
+	let toggleMid = document.getElementsByClassName("toggle-mid");
 
-	if (document.getElementsByClassName("navbar-toggler-icon")[0].style.display == "none"){
-		document.getElementsByClassName("navbar-toggler-icon")[0].style.display = "block";
-		document.getElementsByClassName("navbar-toggler-icon")[1].style.display = "none";
+	if (document.getElementsByClassName("toggle-bottom")[0].style.display == "none"){
+		toggleMid[0].style.transform = "rotate(0deg)";
+		toggleMid[1].style.transform = "rotate(0deg)";
+		document.getElementsByClassName("toggle-bottom")[0].style.display = "block";
+		document.getElementsByClassName("toggle-top")[0].style.display = "block";
 	} else {
-		document.getElementsByClassName("navbar-toggler-icon")[0].style.display = "none";
-		document.getElementsByClassName("navbar-toggler-icon")[1].style.display = "block";
+		toggleMid[0].style.transform = "rotate(45deg)";
+		toggleMid[1].style.transform = "rotate(-45deg)";
+		document.getElementsByClassName("toggle-bottom")[0].style.display = "none";
+		document.getElementsByClassName("toggle-top")[0].style.display = "none";
 	}
 
 })
