7f430547048e7c45696544be8a73e2accb015bcd
Nikhil Singh <nikhilsingh498@gmail.com>
2020-05-03 21:39:39 +0530

    Final getintouch

--- scripts/getintouch.js
+++ scripts/getintouch.js
@@ -42,3 +42,69 @@ document.getElementById('submit').addEventListener('click', (e) => {
       document.getElementById('submit').innerHTML = 'There was some error';
     });
 });
+
+/* eslint-disable no-param-reassign */
+function emailVerify(event) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (event.target === undefined) {
+    if (re.test(event)) {
+      return true;
+    }
+    return false;
+  }
+
+  const email = event.target.value;
+  if (re.test(email)) {
+    event.target.style.borderColor = 'Green';
+    return true;
+  }
+  event.target.style.borderColor = 'Red';
+  return false;
+}
+
+function nameVerify(event) {
+  if (event.target === undefined) {
+    if (event.length > 0) {
+      return true;
+    }
+    return false;
+  }
+
+  const name = event.target.value;
+  if (name.length > 0) {
+    event.target.style.borderColor = 'Green';
+    return true;
+  }
+  event.target.style.borderColor = 'Red';
+  return false;
+}
+
+function messageVerify(event) {
+  if (event.target === undefined) {
+    if (event.length > 0) {
+      return true;
+    }
+
+    return false;
+  }
+
+  const message = event.target.value;
+  if (message.length > 0) {
+    event.target.style.borderColor = 'Green';
+    return true;
+  }
+  event.target.style.borderColor = 'Red';
+  return false;
+}
+
+function activateBtn() {
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('message').value;
+
+  if (emailVerify(email) && nameVerify(name) && messageVerify(message)) {
+    document.getElementById('submit').disabled = false;
+  } else {
+    document.getElementById('submit').disabled = true;
+  }
+}
